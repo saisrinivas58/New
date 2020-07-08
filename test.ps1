0eae63cf097d28b7078c067ffa8483a2b029f14d
saisrinivas <vasa.saisrinivas839@gmail.com>
2020-07-08 19:32:00 +0530

    updated

--- test.ps1
+++ test.ps1
@@ -9,15 +9,15 @@
 [String]$StorageKey="J8CvjuRa5BG1yuULUxj7znnuYVkktGMwyE5/KhBZ+htZP4/qfGI/Jsy+fFOz2d8XKUFjVldeRwKO2kMbzCd8+w=="
 [String]$BlobContainerName="backups"
 [String]$StorageAccessKey="StorageAccessKey"
-$creds = New-Object –TypeName System.Management.Automation.PSCredential($DatabaseAdminUsername, $DatabaseAdminPassword)
+#$creds = New-Object –TypeName System.Management.Automation.PSCredential($DatabaseAdminUsername, $DatabaseAdminPassword)
      
 $bacpacFilename = $DatabaseNames + (Get-Date).ToString("yyyyMMddHHmm") + ".bacpac"
 $bacpacUri = $BlobStorageEndpoint+ "/" + $BlobContainerName+ "/" + $bacpacFilename
  Write-Output "backup files created"   
 #$exportRequest = New-AzureRmSqlDatabaseExport –DatabaseName $DatabaseNames –ServerName $DatabaseServerName –storageKey $StorageKey -storageUri $bacpacUri –AdministratorLogin $creds.UserName –AdministratorLoginPassword $creds.Password -ResourceGroupName $ResourceGroupName
 
-$exportRequest= New-AzureRmSqlDatabaseExport -ResourceGroupName $ResourceGroupName -DatabaseName $DatabaseNames -ServerName $DatabaseServerName -StorageKey $StorageKey -StorageAccessKey $StorageAccessKey -storageUri $bacpacUri -AdministratorLogin $creds.UserName -AdministratorLoginPassword -$creds.Password 
-#$exportRequest= New-AzureRmSqlDatabaseExport -ResourceGroupName $ResourceGroupName -DatabaseName $DatabaseNames -ServerName $DatabaseServerName -StorageKey $StorageKey -StorageAccessKey $StorageAccessKey -storageUri $bacpacUri -AdministratorLogin $DatabaseAdminUsername -AdministratorLoginPassword -$DatabaseAdminPassword          
+#$exportRequest= New-AzureRmSqlDatabaseExport -ResourceGroupName $ResourceGroupName -DatabaseName $DatabaseNames -ServerName $DatabaseServerName -StorageKey $StorageKey -StorageAccessKey $StorageAccessKey -storageUri $bacpacUri -AdministratorLogin $creds.UserName -AdministratorLoginPassword -$creds.Password 
+$exportRequest= New-AzureRmSqlDatabaseExport -ResourceGroupName $ResourceGroupName -DatabaseName $DatabaseNames -ServerName $DatabaseServerName -StorageKey $StorageKey -StorageAccessKey $StorageAccessKey -storageUri $bacpacUri -AdministratorLogin $DatabaseAdminUsername -AdministratorLoginPassword -$DatabaseAdminPassword          
 
 
                             
