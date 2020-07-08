f02b0426278f842e2bb0d60f53d81590687b0fc6
saisrinivas <vasa.saisrinivas839@gmail.com>
2020-07-08 18:44:24 +0530

    updated file

---
+++ test.ps1
@@ -0,0 +1,24 @@
+     [String]$ResourceGroupName="DEVOPS-TRAINING-RG"
+     [String]$DatabaseServerName="mytestdb456"
+     [String]$DatabaseAdminUsername="cproot"
+	 [String]$DatabaseAdminPassword="Password@"
+	 #[String]$DatabaseAdminPassword= ConvertTo-SecureString -String "Password@" –AsPlainText -Force 
+	 [String]$DatabaseNames="Mydb"
+     [String]$StorageAccountName="devopstraineestroage"
+     [String]$BlobStorageEndpoint="https://devopstraineestroage.blob.core.windows.net"
+     [String]$StorageKey="J8CvjuRa5BG1yuULUxj7znnuYVkktGMwyE5/KhBZ+htZP4/qfGI/Jsy+fFOz2d8XKUFjVldeRwKO2kMbzCd8+w=="
+	 [String]$BlobContainerName="backups"
+	 [String]$StorageAccessKey="StorageAccessKey"
+	 $creds = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $DatabaseAdminUsername, $DatabaseAdminPassword
+     
+	 $bacpacFilename = $DatabaseNames + (Get-Date).ToString("yyyyMMddHHmm") + ".bacpac"
+	 $bacpacUri = $BlobStorageEndpoint+ "/" + $BlobContainerName+ "/" + $bacpacFilename
+        Write-Output "backup files created"   
+		 $exportRequest = New-AzureRmSqlDatabaseExport `
+                                                –DatabaseName $DatabaseNames `
+                                                –ServerName $DatabaseServerName`
+                                                –storageKey $StorageKey`
+                                                -StorageUri $bacpacUri`
+                                                –AdministratorLogin $creds.UserName `
+                                                –AdministratorLoginPassword $creds.Password`
+                                                -ResourceGroupName $ResourceGroupName
